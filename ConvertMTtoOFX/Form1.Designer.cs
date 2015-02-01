61200d7aab585aae023c2fd7c5933aa4b14f7f47
Richard Vrijhof <r@vrijhof.info>
2015-02-01 17:05:31 +0100

    First import

---
+++ ConvertMTtoOFX/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ConvertMTtoOFX {
+  partial class Form1 {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.button1 = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      this.tableLayoutPanel1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+      this.button1.AutoSize = true;
+      this.button1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button1.Location = new System.Drawing.Point(79, 85);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 38);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "OK";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // label1
+      // 
+      this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.Location = new System.Drawing.Point(3, 11);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(227, 19);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "File is converted. Enjoy!";
+      this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // label2
+      // 
+      this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+      this.label2.AutoSize = true;
+      this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label2.Location = new System.Drawing.Point(3, 52);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(227, 19);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "label2";
+      this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      this.label2.Visible = false;
+      // 
+      // tableLayoutPanel1
+      // 
+      this.tableLayoutPanel1.ColumnCount = 1;
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+      this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+      this.tableLayoutPanel1.Controls.Add(this.button1, 0, 2);
+      this.tableLayoutPanel1.Location = new System.Drawing.Point(2, 0);
+      this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+      this.tableLayoutPanel1.RowCount = 3;
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+      this.tableLayoutPanel1.Size = new System.Drawing.Size(233, 126);
+      this.tableLayoutPanel1.TabIndex = 3;
+      // 
+      // Form1
+      // 
+      this.AcceptButton = this.button1;
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.AutoSize = true;
+      this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+      this.ClientSize = new System.Drawing.Size(235, 126);
+      this.Controls.Add(this.tableLayoutPanel1);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "ConvertMTtoOFX";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+      this.tableLayoutPanel1.ResumeLayout(false);
+      this.tableLayoutPanel1.PerformLayout();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+
+  }
+}
+
---
+++ ConvertMTtoOFX/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertMTtoOFX {
+  public partial class Form1 : Form {
+    public Form1() {
+      InitializeComponent();
+
+      OpenFileDialog InputFileBrowser = new OpenFileDialog();
+      InputFileBrowser.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + @"\Downloads";
+      InputFileBrowser.Filter = "SWIFT MT940 files (*.sta)|*.sta";
+      InputFileBrowser.Title = "Select MT940 input file";
+      InputFileBrowser.ShowDialog();
+      if (InputFileBrowser.FileName == "") {
+        Environment.Exit(1);
+      }
+
+      SaveFileDialog OutputFileBrowser = new SaveFileDialog();
+      OutputFileBrowser.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + @"\Downloads";
+      OutputFileBrowser.Filter = "Open Financial Exchange file (*.ofx)|*.ofx";
+      OutputFileBrowser.Title = "Save as OFX file";
+      OutputFileBrowser.ShowDialog();
+      if (OutputFileBrowser.FileName == "") {
+        Environment.Exit(2);
+      }
+
+      if (InputFileBrowser.CheckFileExists && OutputFileBrowser.CheckPathExists) {
+        ProcessStartInfo ProcessInfo = new ProcessStartInfo();
+        ProcessInfo.FileName = "java";
+        ProcessInfo.UseShellExecute = false;
+        ProcessInfo.CreateNoWindow = true;
+        ProcessInfo.RedirectStandardError = true;
+        ProcessInfo.Arguments = "-jar C:\\Windows\\mtconverter.jar \"" + InputFileBrowser.FileName + "\" \"" + OutputFileBrowser.FileName + "\"";
+        Process Java = new Process();
+        Java.StartInfo = ProcessInfo;
+        Java.Start();
+        String Output = Java.StandardError.ReadToEnd();
+        Java.WaitForExit();
+        if (Output != "") {
+          label1.Text = "Conversion went wrong. Sorry!";
+          Regex RX = new Regex(@"\(([^\)]+)\)\r\n");
+          MatchCollection Matches = RX.Matches(Output);
+          if (Matches.Count > 0) {
+            GroupCollection Groups = Matches[0].Groups;
+            String Error = Groups[1].Value;
+            label2.Text = "Error: " + Error;
+            label2.Visible = true;
+            CenterControlInParent(label2);
+          }
+        } else {
+          this.Controls.Remove(label2);
+          tableLayoutPanel1.RowStyles[1].Height = 0;
+        }
+      }
+    }
+
+    private void button1_Click(object sender, EventArgs e) {
+      Environment.Exit(0);
+    }
+
+    private void Form1_Load(object sender, EventArgs e) {
+      this.Activate();
+    }
+
+    private void Form1_SizeChanged(object sender, EventArgs e) {
+      CenterControlInParent(label1);
+    }
+
+    private void CenterControlInParent(Control ctrlToCenter) {
+      ctrlToCenter.Left = (ctrlToCenter.Parent.Width - ctrlToCenter.Width) / 2;
+      ctrlToCenter.Anchor = AnchorStyles.None;
+    }
+
+  }
+}
